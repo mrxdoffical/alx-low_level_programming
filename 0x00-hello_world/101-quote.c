a350ed56d0ab65d3282b8631918c865c078cab61
bero <berogergesg@gmail.com>
2023-08-10 23:43:59 +0000

    aded task101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+*main - main function
+*
+*Return: 0 for success and 1 for failure
+*/
+
+int main(void)
+{
+		char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+				write(1, q, 59);
+
+					return (1);
+}
