69b7cb95ec91a6dbb4f50490ddde21dd39370fc8
mrxdoffical <berogergesg@gmail.com>
2023-08-16 03:06:17 +0300

    added task1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - entry point
+ * 
+ * description: prints _putchar using putchar prototype
+ * 
+ * return: always 0 (successs)
+*/
+int main(void)
+	char str [] = "_putchar";
+	int ch;
+for (ch = 0; ch 8: ch++)
+	_putchar(str[ch]);
+_putchar('\n');
+
+return (0);
