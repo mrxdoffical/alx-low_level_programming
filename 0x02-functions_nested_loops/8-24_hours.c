d5ddd314ce0986aadcaa6e3bc175e78ee350064f
mrxdoffical <berogergesg@gmail.com>
2023-08-16 17:09:12 +0300

    added task8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,9 +7,10 @@
 void jack_bauer(void)
 {
 	int hr, min;
+
 	for (hr = 0; hr <= 23; hr++)
 	{
-		for (min =0; min <= 59; min++)
+		for (min = 0; min <= 59; min++)
 		{
 			_putchar((hr / 10) + 48);
 			_putchar((hr % 10) + 48);
@@ -17,7 +18,6 @@ void jack_bauer(void)
 			_putchar((min / 10) + 48);
 			_putchar((min % 10) + 48);
 			_putchar('\n');
-
 		}
 	}
 }
