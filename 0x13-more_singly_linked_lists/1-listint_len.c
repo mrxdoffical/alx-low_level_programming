dbc22a33e75cbbaccd33f24d6a3acdae8bcaf703
mrxdoffical <berogergesg@gmail.com>
2023-09-26 13:20:04 +0300

    added task1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,16 +1,17 @@
 #include"lists.h"
 /**
 * listint_len - prints a linked lists
-*@h: pointers to first node
-*return size of list
+* @h: pointers to first node
+*return (always return success) vlau size of list
 */
 size_t listint_len(const listint_t *h)
 {
-	size_t i =0;
+	size_t i = 0;
+
 	while (h)
 	{
 		h = h->next;
 		i++;
 	}
-	return(i);
+	return (i);
 }
