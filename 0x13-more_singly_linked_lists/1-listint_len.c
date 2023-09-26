f3d3ca4f40375f6ea203f47ae02647d2357fce6c
mrxdoffical <berogergesg@gmail.com>
2023-09-26 13:28:18 +0300

    added task1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,9 +1,11 @@
 #include"lists.h"
 /**
-* listint_len - prints a linked lists
-* @h: pointers to first node
-*return (always return success) vlau size of list
-*/
+ * listint_len - returns the number of elements in a linked lists
+ * @h: linked list of type listint_t to traverse
+ *
+ * Return: number of nodes
+ */
+
 size_t listint_len(const listint_t *h)
 {
 	size_t i = 0;
