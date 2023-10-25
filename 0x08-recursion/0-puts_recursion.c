486229a9114c81397c6545d1250e29bc1ae99d9d
mrxdoffical <berogergesg@gmail.com>
2023-10-25 20:56:01 +0300

    added task

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,15 +9,15 @@
   * Return: Always 0.
   */
 
-    void _puts_recursion(char *str)
-    {
-        if(*str=='\0')
-        {
-            _putchar('\n');
-        }
-        else
-        {
-            _putchar(*str);
-            _puts_recursion(str + 1);
-        }
-    }
+	void _puts_recursion(char *str)
+	{
+		if(*str=='\0')
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar(*str);
+			_puts_recursion(str + 1);
+		}
+	}
