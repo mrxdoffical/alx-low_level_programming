8454eeb714e29acf9d569588e8331c1e48d94c66
mrxdoffical <berogergesg@gmail.com>
2023-10-30 19:19:18 +0200

    first task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,9 +3,10 @@
  *main - function prints argv[]
  * @argc : int
  * @argv : list
+ * return 0
 */
 
-int main(int argc , char* argv[])
+int main(int argc , char const *argv[])
 {
     (void)argc;
     printf("%s\n",argv[0]);
