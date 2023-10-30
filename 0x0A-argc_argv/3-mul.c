78bf24ad7bf160fd874bb8c8f915aead17de1cbd
mrxdoffical <berogergesg@gmail.com>
2023-10-30 21:55:32 +0200

    added tasks

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - multiplies two numbers
+ * _atoi - converts a string to an integer
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 (Success), 1 (Error)
+ */
+int  main(int argc, char *argv[])
+{
+	int result = 1;
+
+		if (argc == 3)
+		{
+		result = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", result);
+		}
+		else
+		printf("error\n");
+
+	return (0);
+}
