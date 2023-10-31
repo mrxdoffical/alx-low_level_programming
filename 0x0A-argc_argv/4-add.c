21963b309dd28dcab278df195b8c0b45ec98b85e
mrxdoffical <berogergesg@gmail.com>
2023-10-31 18:26:38 +0200

    added tasks

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,18 +1,26 @@
 #include<stdio.h>
 #include<stdlib.h>
+/**
+ * main - Print the name of the program
+* @argc: Count arguments
+* @argv: Arguments
+* Return: Always 0 (Success)
+*/
+
 int main(int argc, char *argv[])
 {
 int result = 0;
 char *inp;
-while(--argc)
+while (--argc)
 {
-	for(inp=argv[argc]; *inp; inp++)
+	for (inp = argv[argc]; *inp; inp++)
 	{
-		if(*inp < '0' || *inp > '9')
-		return(printf("Error\n"),1);
+		if (*inp < '0' || *inp > '9')
+		return (printf("Error\n"), 1);
 		result = atoi(argv[argc]);
 	}
-	printf("%d\n",result);
+	printf("%d\n", result);
 	return (0);
 }
 }
+
