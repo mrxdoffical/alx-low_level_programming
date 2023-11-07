8bacfb7f8e812b1fbb7560dca586bf7945ac209f
mrxdoffical <berogergesg@gmail.com>
2023-11-07 22:35:43 +0200

    added file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * @brief This is the main function in C that prints the name of the source file and returns 0.
+ *
+ * @return int Returns 0 to indicate successful execution.
+ */
+int main()
+{
+    printf("%s\n",__FILE__);
+    return(0);
+}
