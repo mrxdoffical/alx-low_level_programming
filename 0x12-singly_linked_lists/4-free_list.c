295e181b349abde85cab9071ff1844ce37ad5b0d
mrxdoffical <berogergesg@gmail.com>
2023-11-16 23:29:44 +0200

    added tasks

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -5,11 +5,17 @@
 */
 void free_list(list_t *head)
 {
-	list_t *node = head;
+	list_t *node, *next_node;
 
-	while ((node = node->next) != NULL)
+	if (!head)
+	return;
+
+	node = head;
+	while (node)
 	{
+		next_node = node->next;
 		free(node->str);
 		free(node);
+		node = next_node;
 	}
 }
