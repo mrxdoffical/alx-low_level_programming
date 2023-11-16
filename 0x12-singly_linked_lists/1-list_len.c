e8c9da50a30f9b431a90b50c3bb65779ff022826
mrxdoffical <berogergesg@gmail.com>
2023-11-16 22:19:36 +0200

    added tasks

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,16 @@
+#include"lists.h"
+
+
+
+size_t list_len(const list_t *h)
+{
+   size_t count = 0;
+	const list_t *current = h;
+
+	while (current != NULL) {
+		count++;
+		current = current->next;
+	}
+
+	return count;
+}
