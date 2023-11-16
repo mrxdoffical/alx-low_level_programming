b32002801468e876e788f9f9b4d3c76e7191eb87
mrxdoffical <berogergesg@gmail.com>
2023-11-16 22:23:03 +0200

    added tasks

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,16 +1,21 @@
 #include"lists.h"
-
+/**
+ * list_len -  a function that returns the number of elements in a linked
+ * @h: pointer
+ * Return: counter
+*/
 
 
 size_t list_len(const list_t *h)
 {
-   size_t count = 0;
+	size_t count = 0;
 	const list_t *current = h;
 
-	while (current != NULL) {
+	while (current != NULL)
+	{
 		count++;
 		current = current->next;
 	}
 
-	return count;
+	return (count);
 }
