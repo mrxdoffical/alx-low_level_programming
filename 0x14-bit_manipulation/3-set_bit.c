d749716819ab4c654a3c4deb75c5bab0d79d9c01
mrxdoffical <berogergesg@gmail.com>
2023-11-23 14:53:14 +0200

    added task

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 #include"main.h"
 
+/**
+ * set_bit - sets the bit at the index
+ * @n: the num to index
+ * @index: the bit set
+ * Return: 1 if successful -1 on error
+*/
+
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= sizeof(n) * 8)
