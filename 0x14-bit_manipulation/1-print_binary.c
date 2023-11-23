df3e8afc54392e70fc3d19e6bd89d7e02bc818e4
mrxdoffical <berogergesg@gmail.com>
2023-11-23 14:30:59 +0200

    added tasks

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,17 +9,17 @@
 
 void print_binary(unsigned long int n)
 {
-	int bit = sizeof(n) *, printed = 0;
+	int bit = sizeof(n) * 8, printed = 0;
 
 	while (bit)
 	{
 		if (n & 1L << --bit)
 		{
 			_putchar('1');
-			printed++ ('0');
+			printed++ ;
 		}
 		else if (printed)
-		_putchar('0')
+		_putchar('0');
 	}
 	if (!printed)
 	_putchar('0');
