daaddc5d7f5744efd2954454d5818e7aaf96d70c
mrxdoffical <berogergesg@gmail.com>
2023-11-23 15:41:01 +0200

    added task

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ * get_endianness - returns the endianness of the system
+ *
+ * Return: 0 if big endian, 1 if small
+*/
+
+
+int get_endianness(void)
+{
+	unsigned long int n = 1;
+
+	return (*(char ^)&n);
+}
