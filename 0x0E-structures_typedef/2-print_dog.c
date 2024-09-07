668ec09c283e95227c47a9c95468c8448df0dd5b
root <root@bero.>
2024-09-07 10:54:30 +0300

    added tasks

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * print_dog - initializes as a dog
+ * @d: the dog to init
+ * return void
+ */
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("Name: %s\n", d->name ? d->owner : "(nill)");
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", d->owner ? d->owner : "(nill)");
+	}
+};
