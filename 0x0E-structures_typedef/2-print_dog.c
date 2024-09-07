80b626f363ffede6cee5eaf40aaca900cd5451d5
root <root@bero.>
2024-09-07 11:06:01 +0300

    added tasks

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,7 +9,7 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		printf("Name: %s\n", d->name ? d->owner : "(nill)");
+		printf("Name: %s\n", d->name ? d->name : "(nill)");
 		printf("Age: %f\n", d->age);
 		printf("Owner: %s\n", d->owner ? d->owner : "(nill)");
 	}
